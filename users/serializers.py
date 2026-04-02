ff71ac7b78a4bc9ee672f522a2aa3b94cdd23082
TheNucleya02 <kr.amanjha02@gmail.com>
2026-04-02 16:27:13 +0530

    fix: restrict user serializer fields and correctly hash password on register

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,24 @@ from .models import User
 class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('__all__')
+        fields = (
+            'id', 'username', 'email', 'password',
+            'first_name', 'last_name', 'phonenumber',
+            'bio', 'profile_picture', 'certification', 'specialization'
+        )
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True},
+            'phonenumber': {'required': False, 'default': '0000000000'},
+            'bio': {'required': False},
+            'profile_picture': {'required': False},
+            'certification': {'required': False},
+            'specialization': {'required': False},
         }
 
     def create(self, validated_data):
+        # Pop password before creating the object so it is never stored as plain text
+        password = validated_data.pop('password')
         user = User(**validated_data)
-        user.set_password(validated_data['password'])
+        user.set_password(password)
         user.save()
         return user
